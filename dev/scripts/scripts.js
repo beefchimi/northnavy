cc0d20b0357fb23a535bba3ab14cc718ee3f877b
beefchimi <curtdulmage@gmail.com>
2015-02-08 14:29:22 -0500

    beginning packery gallery

--- dev/scripts/scripts.js
+++ dev/scripts/scripts.js
@@ -233,11 +233,61 @@ document.addEventListener('DOMContentLoaded', function() {
 	}
 
 
+	// layoutPackery: Wait until images are loaded then layout with packery.js
+	// ----------------------------------------------------------------------------
+	function layoutPackery() {
+
+		var elPackeryContainer = document.getElementById('packery'),
+			arrImages          = document.getElementsByClassName('gallery_link');
+
+		// layout Packery after all images have loaded
+		imagesLoaded(elPackeryContainer, function(instance) {
+
+			// initalize packery
+			var elPackery = new Packery(elPackeryContainer, {
+				itemSelector: 'a.gallery_link',
+				gutter: 'div.gutter-sizer',
+				transitionDuration: '0.6s',
+				visibleStyle: {
+					opacity: 1,
+					transform: 'scale(1)'
+				},
+				hiddenStyle: {
+					opacity: 0,
+					transform: 'scale(1)'
+				}
+			});
+
+			// hide loader by removing 'visible' class
+			// classie.remove(elLoader, 'visible');
+
+			// iterate through each article and add 'loaded' class once ready
+			for (var i = 0; i < arrImages.length; i++) {
+				(function(i){
+					setTimeout(function() {
+						classie.add(arrImages[i], 'loaded');
+					}, 200 * i);
+				})(i)
+			}
+
+			// once our images have loaded, it is safe to initialize gallery()
+			// gallery();
+
+		});
+
+	}
+
+
+
+
 	// Initialize Primary Functions
 	// ----------------------------------------------------------------------------
 
 	// navToggle();
 	secretEmail();
+	layoutPackery();
+
+
 	// toggleModal();
 
 
