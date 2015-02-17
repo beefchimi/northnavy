4305381eada41c3109346ff49b1ef267fb535b4a
beefchimi <curtdulmage@gmail.com>
2015-02-17 16:38:49 -0500

    implemented viewport resizing draft

--- dev/scripts/scripts.js
+++ dev/scripts/scripts.js
@@ -1,6 +1,96 @@
 document.addEventListener('DOMContentLoaded', function() {
 
 
+	// Global Variables: Variables required for more than 1 function
+	// ----------------------------------------------------------------------------
+	var elBody           = document.body,
+		elHeaderLogo     = document.getElementById('resize_logo'),
+		elHeaderRule     = document.getElementById('resize_rule'),
+		elHeaderWordmark = document.getElementById('resize_wordmark'),
+		elWrapMenu       = document.getElementById('wrap_menu'),
+		elMenuLunch      = document.getElementById('menu_lunch'),
+		elMenuDinner     = document.getElementById('menu_dinner');
+
+	var numWindowWidth,
+		numWindowHeight,
+		numRatio,
+		numThreshold,
+		numLogoMargin,
+		numRuleMargin,
+		numWordmarkMargin,
+		numLogoWidth,
+		numFullWidth;
+
+
+	// Helper: Fire Window Resize Event Upon Finish
+	// ----------------------------------------------------------------------------
+	var waitForFinalEvent = (function() {
+
+		var timers = {};
+
+		return function(callback, ms, uniqueId) {
+
+			if (!uniqueId) {
+				uniqueId = 'beefchimi'; // Don't call this twice without a uniqueId
+			}
+
+			if (timers[uniqueId]) {
+				clearTimeout(timers[uniqueId]);
+			}
+
+			timers[uniqueId] = setTimeout(callback, ms);
+
+		};
+
+	})();
+
+
+	// Helper: Check when a CSS transition or animation has ended
+	// ----------------------------------------------------------------------------
+	function whichTransitionEvent() {
+
+		var trans,
+			element     = document.createElement('fakeelement'),
+			transitions = {
+				'transition'       : 'transitionend',
+				'OTransition'      : 'oTransitionEnd',
+				'MozTransition'    : 'transitionend',
+				'WebkitTransition' : 'webkitTransitionEnd'
+			}
+
+		for (trans in transitions) {
+			if (element.style[trans] !== undefined) {
+				return transitions[trans];
+			}
+		}
+
+	}
+
+/*
+	function whichAnimationEvent() {
+
+		var anim,
+			element    = document.createElement('fakeelement'),
+			animations = {
+				'animation'       : 'animationend',
+				'OAnimation'      : 'oAnimationEnd',
+				'MozAnimation'    : 'animationend',
+				'WebkitAnimation' : 'webkitAnimationEnd'
+			}
+
+		for (anim in animations) {
+			if (element.style[anim] !== undefined) {
+				return animations[anim];
+			}
+		}
+
+	}
+*/
+
+	var transitionEvent = whichTransitionEvent(); // listen for a transition
+	// var animationEvent  = whichAnimationEvent(); // listen for a animation
+
+
 	// secretEmail: Add mailto link to footer
 	// ----------------------------------------------------------------------------
 	function secretEmail() {
@@ -38,6 +128,125 @@ document.addEventListener('DOMContentLoaded', function() {
 
 	}
 */
+
+
+	// viewportHeader: Resize header elements to fit within the viewport
+	// ----------------------------------------------------------------------------
+	function viewportHeader() {
+
+		// measure window width on load and resize
+		numWindowWidth = window.innerWidth;
+
+		if (numWindowWidth >= 912) {
+
+			// calculate our sizing values
+			numWindowHeight   = window.innerHeight;
+			numRatio          = numWindowHeight / 1100; // (912 / 2) + 912 = 1368
+			numThreshold      = numRatio >= 1 ? 1 : numRatio;
+			numLogoMargin     = 80  * numThreshold; // parseFloat( window.getComputedStyle(elHeaderLogo, null).getPropertyValue('margin-bottom') )
+			numRuleMargin     = 90 * numThreshold;
+			numWordmarkMargin = 140 * numThreshold;
+			numLogoWidth      = 120 * numThreshold;
+			numFullWidth      = 912 * numThreshold;
+
+			// apply updated inline styles
+			elHeaderLogo.style.marginBottom = numRuleMargin + 'px';
+			// elHeaderLogo.style.cssText     += '; width:' + numLogoWidth + 'px; margin-bottom:' + numRuleMargin + 'px';
+			elHeaderRule.style.cssText     += '; width:' + numFullWidth + 'px; margin-bottom:' + numRuleMargin + 'px';
+			elHeaderWordmark.style.cssText += '; width:' + numFullWidth + 'px; margin-bottom:' + numWordmarkMargin + 'px';
+
+		} else {
+
+			// in case we are resizing down, remove any inline styles we may have set
+			elHeaderLogo.removeAttribute('style');
+			elHeaderRule.removeAttribute('style');
+			elHeaderWordmark.removeAttribute('style');
+
+		}
+
+	}
+
+
+	// menuToggle: Lunch & Dinner menu toggle
+	// ----------------------------------------------------------------------------
+	function menuToggle() {
+
+		var elLunchToggle  = document.getElementById('toggle_lunch'),
+			elDinnerToggle = document.getElementById('toggle_dinner'),
+			targetToggle,
+			targetEvent;
+
+		// toggle the Lunch menu
+		elLunchToggle.addEventListener('click', function(e) {
+
+			if ( !classie.has(this, 'toggled') ) {
+
+				targetToggle = elMenuLunch;
+				targetEvent  = elMenuDinner;
+
+				elWrapMenu.setAttribute('data-menu', 'lunch');
+
+				classie.add(this, 'toggled');
+				classie.remove(elMenuDinner, 'toggled');
+				classie.remove(elDinnerToggle, 'toggled');
+
+				targetEvent.addEventListener(transitionEvent, menuTrackTransition);
+
+			}
+
+			e.preventDefault();
+
+		});
+
+		// toggle the Dinner menu
+		elDinnerToggle.addEventListener('click', function(e) {
+
+			if ( !classie.has(this, 'toggled') ) {
+
+				targetToggle = elMenuDinner;
+				targetEvent  = elMenuLunch;
+
+				elWrapMenu.setAttribute('data-menu', 'dinner');
+
+				classie.add(this, 'toggled');
+				classie.remove(elMenuLunch, 'toggled');
+				classie.remove(elLunchToggle, 'toggled');
+
+				targetEvent.addEventListener(transitionEvent, menuTrackTransition);
+
+			}
+
+			e.preventDefault();
+
+		});
+
+		// wait for CSS opacity transition to end
+		function menuTrackTransition() {
+
+			menuHeight();
+			classie.add(targetToggle, 'toggled');
+
+			// must remove event listener!
+			targetEvent.removeEventListener(transitionEvent, menuTrackTransition);
+
+		}
+
+	}
+
+
+	// menuHeight: Apply Lunch & Dinner menu height
+	// ----------------------------------------------------------------------------
+	function menuHeight() {
+
+		var strDefaultMenu = elWrapMenu.getAttribute('data-menu'),
+			numMenuLunch   = elMenuLunch.clientHeight,
+			numMenuDinner  = elMenuDinner.clientHeight,
+			numNewHeight   = strDefaultMenu === 'lunch' ? numMenuLunch : numMenuDinner;
+
+		// set height of div.wrap_wrap to toggled div.menu_list
+		elWrapMenu.style.height = numNewHeight + 'px';
+
+	}
 
 
 	// layoutPackery: Wait until images are loaded then layout with packery.js
@@ -79,9 +288,39 @@ document.addEventListener('DOMContentLoaded', function() {
 	}
 
 
+	// Window Events: On - Scroll, Resize
+	// ----------------------------------------------------------------------------
+/*
+	window.addEventListener('scroll', function(e) {
+
+		updateColor();
+
+	}, false);
+*/
+
+
+
+	window.addEventListener('resize', function(e) {
+
+		// do not fire resize event for every pixel... wait until finished
+		waitForFinalEvent(function() {
+
+			menuHeight();
+			viewportHeader();
+
+		}, 500, 'unique string');
+
+	}, false);
+
+
+
+
 	// Initialize Primary Functions
 	// ----------------------------------------------------------------------------
 	secretEmail();
+	viewportHeader();
+	menuToggle();
+	menuHeight();
 	layoutPackery();
 
 	smoothScroll.init({
